0bbeeea3c3d736456a9e1d013cca32df95533c23
Kiran Duggirala <kduggirala10@stuy.edu>
2019-03-22 10:46:29 -0400

    Ran Driver: got NullPointerException

--- Driver.java
+++ Driver.java
@@ -1,6 +1,12 @@
 
 public class Driver {
 	public static void main(String[] args) {
-		
+		MyDeque<Integer> q = new MyDeque<Integer>();
+		System.out.println(q);
+		for (int i = 0; i < 10; i++) {
+			q.addFirst(i);
+			q.addLast(i);
+		}
+		System.out.println(q);
 	}
 }
