af5b32dd319e3f41e69c72089b698a9088eb63fe
Kiran Duggirala <kduggirala10@stuy.edu>
2019-03-25 10:53:12 -0400

    Tested eval method: works on given test strings

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,12 @@ public class Calculator{
 	/*Evaluate a postfix expression stored in s.
 	 *Assume valid postfix notation, separated by spaces.
 	 */
+	public static void main(String[] args) {
+		System.out.println(eval("10 2.0 +"));
+		System.out.println(eval("11 3 - 4 + 2.5 *"));
+		System.out.println(eval("8 2 + 99 9 - * 2 + 9 -"));
+		System.out.println(eval("1 2 3 4 5 + * - -"));
+	}
 	public static double eval(String s){
 		String[] string = s.split(" ");
 		MyDeque<Double> stack = new MyDeque<Double>();
