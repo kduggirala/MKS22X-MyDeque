9899597db6e756521a57a0723564a66f63ecd296
Kiran Duggirala <kduggirala10@stuy.edu>
2019-03-25 10:49:52 -0400

    Wrote eval method

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,45 @@
 
 public class Calculator{
-    /*Evaluate a postfix expression stored in s.
-     *Assume valid postfix notation, separated by spaces.
-     */
-    public static double eval(String s){
-    	
-    }
+	/*Evaluate a postfix expression stored in s.
+	 *Assume valid postfix notation, separated by spaces.
+	 */
+	public static double eval(String s){
+		String[] string = s.split(" ");
+		MyDeque<Double> stack = new MyDeque<Double>();
+		double second, first;
+		for (int i = 0; i < string.length; i++) {
+			switch(string[i]) {
+			case "+":
+				second = stack.removeLast();
+				first = stack.removeLast();
+				stack.addLast(first + second);
+				break;
+			case "-":
+				second = stack.removeLast();
+				first = stack.removeLast();
+				stack.addLast(first + second);
+				break;
+			case "*":
+				second = stack.removeLast();
+				first = stack.removeLast();
+				stack.addLast(first + second);
+				break;
+			case "/":
+				second = stack.removeLast();
+				first = stack.removeLast();
+				stack.addLast(first + second);
+				break;
+			case "%":
+				second = stack.removeLast();
+				first = stack.removeLast();
+				stack.addLast(first + second);
+				break;
+			default:
+				stack.addLast(Double.parseDouble(string[i]));
+				break;
+			}
+		}
+		return stack.getLast();
+	}
 }
 
